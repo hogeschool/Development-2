7e306f18d0257a7bace0d3b047b4872253933aa1
mabbadi <mabbadi@outlook.com>
2015-12-07 16:19:39 +0100

    Assignment 1 updated

---
+++ Assignments/Assignment 1/DEV 2 Game template (py 3_4)/DEV 2 Game template/Node.py
@@ -0,0 +1,11 @@
+﻿class Node:
+  def __init__(self, value, tail):
+    self.Tail = tail
+    self.Value = value
+    self.IsEmpty = False
+
+class Empty: 
+  def __init__(self):
+    self.IsEmpty = True
+
+Empty = Empty()
