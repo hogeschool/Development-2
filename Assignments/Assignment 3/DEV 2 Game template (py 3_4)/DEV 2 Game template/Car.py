1dfd9bdb0f61b20d20d4db3e32044eec72d27509
mabbadi <mabbadi@outlook.com>
2016-01-05 10:22:37 +0100

    assignment 3

---
+++ Assignments/Assignment 3/DEV 2 Game template (py 3_4)/DEV 2 Game template/Car.py
@@ -0,0 +1,8 @@
+﻿import pygame
+import random
+from Node import *
+from Common import *
+
+#CAR CODE HERE
+
+#NOTE: TO DRAW USE THE CODE AS IN ASSIGNMENT 1
---
+++ Assignments/Assignment 3/DEV 2 Game template (py 3_4)/DEV 2 Game template/Game.py
@@ -0,0 +1,52 @@
+﻿import time
+from threading import Thread
+import os, pygame
+import time
+from Tile import *
+from Node import *
+from Boat import *
+pygame.init()
+size = width, height = 600, 600
+white = 255, 255, 255
+green = 50, 255, 100
+screen = pygame.display.set_mode(size)
+offset = 50
+size = 10
+entry_road, entry_rivers, bridges = build_scene(size, offset)
+
+#faces to the right
+boat_texture = pygame.image.load("Content/tanker.png").convert_alpha()
+
+#faces to the right
+car_texture = pygame.image.load("Content/car.png").convert_alpha()
+
+
+
+
+
+def Main():
+  start = time.time()
+  while True:    
+    pygame.event.pump() 
+    screen.fill(green)
+
+    #here we draw the board, do not move
+    _board = entry_road
+    while not _board.IsEmpty:
+      _board.Value.Draw(screen, False)
+      _board = _board.Tail
+
+    #here we draw the bridges, do not move
+    _board = bridges
+    while not _board.IsEmpty:
+      _board.Value.Draw(screen, True)
+      _board = _board.Tail
+
+    
+
+
+
+    pygame.display.flip()
+    time.sleep(0.2)
+    
+Main()
