55780a2d7dc07c33d5c77715177a844f32a17c95
aomar1 <omara@145-24-235-37.wlan.hro.nl>
2016-01-18 15:17:01 +0100

    Added mac version

---
+++ Assignments/Assignment 2/DEV 2 Game template MAC/Node.py
@@ -0,0 +1,29 @@
+﻿import random
+class Node:
+  def __init__(self, value, tail):
+    self.Tail = tail
+    self.Value = value
+    self.IsEmpty = False
+
+class Empty: 
+  def __init__(self):
+    self.IsEmpty = True
+
+Empty = Empty()
+
+def AUX_length(l, acc):
+  if l.IsEmpty: return acc
+  else: return AUX_length(l.Tail, acc + 1)
+
+def length(l):
+  return AUX_length(l, 0)
+
+def select_one_random(l):
+  _length = length(l)
+  rnd_num = int(random.uniform(0, _length ))
+  while(rnd_num > 0):
+    l = l.Tail
+    rnd_num -= 1
+  return l.Value
+  
+
